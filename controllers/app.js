a9105244c250efddc5b0a602610f69af2302ee1b
dipu-bd <dipu.sudipta@gmail.com>
2016-05-25 21:48:03 +0600

    display data from apigee

--- controllers/app.js
+++ controllers/app.js
@@ -1,6 +1,7 @@
 (function () {
     var APIGEE_ORGNAME = 'sdipu';
     var APIGEE_APPNAME = 'problemsolvinglist';
+    var PROBLEMS_TYPE = 'problems';
 
     /*********************************************************
      * ProblemListApp and
@@ -9,50 +10,119 @@
     var problemListApp = angular.module('ProblemListApp', []);
 
     var homepage = {};
+    var apiClient = null;
 
     problemListApp.controller('HomePageController', function ($scope) {
         homepage = $scope;
-        $scope.config = getConfig();
+        homepage.problems = [];
+        homepage.categories = [];
+        homepage.config = getConfig();
         // download and show list of problems
-        getProblems();
-        getCategory();
+        loadClient(function (client) {
+            apiClient = client;
+            getProblems();
+        });
+        // define functions
+        $scope.deleteProblem = deleteProblem;
+        $scope.addProblem = addProblem;
+        $scope.viewCategory = getProblems;
     });
 
     /*********************************************************
      * APIGEE Client.
-     * CRUD using apigee client
+     * CRUD using Apigee client
      **********************************************************/
-    var client = new Apigee.Client({
-        orgName: APIGEE_ORGNAME,
-        appName: APIGEE_APPNAME
-    });
+    function loadClient(callback) {
+        callback(new Apigee.Client({
+            orgName: APIGEE_ORGNAME,
+            appName: APIGEE_APPNAME,
+            logging: false, //optional - turn on logging, off by default
+            buildCurl: false //optional - log network calls in the console, off by default
+        }));
+    }
 
-    function getProblems() {
-        var options = { 
-            endpoint: "users", //the collection to query
-            qs: {ql: "status='active'", limit: 5}
+    function getProblems(category) {
+        var properties = {
+            type: PROBLEMS_TYPE
         };
+        if (category) {
+            properties.category = category;
+        }
+        /* We pass our properties to getEntity(), which initiates our GET request: */
+        apiClient.getEntity(properties, function (errorStatus, response, errorMessage) {
+            if (errorStatus) {
+                // Error - there was a problem creating the entity
+                alert("Error! Unable to create your entity. "
+                    + "See console to view detailed error message.");
+                console.log(errorMessage);
+            } else {
+                // Success - the entities received
+                console.log(response);
 
-        client.getEntity(options, function (error, data) {
-            if(error) {
-                console.log(error);
-            }
-            else {
-                console.log(data);
+                homepage.problems = response.entities;
+                buildCategory();
             }
         });
     }
 
-    function getCategory() {
-        var client = getClient();
+    function buildCategory() {
+        var data = {};
+        homepage.problems.forEach(function (prob) {
+            formatProblem(prob);
+            if (prob.category) {
+                data[prob.category] = prob.category;
+            }
+        });
+        homepage.categories = Object.getOwnPropertyNames(data);
     }
 
-    function addProblem(link, category) {
-        var client = getClient();
+    function addProblem(prob) {
+        // define data type
+        formatProblem(prob);
+        prob.type = PROBLEMS_TYPE;
+        // send request to api client
+        apiClient.createEntity(prob, function (errorStatus, response, errorMessage) {
+            if (errorStatus) {
+                // Error - there was a problem creating the entity
+                alert("Error! Unable to create your entity. "
+                    + "Did you enter the correct organization name?");
+                console.log(errorMessage);
+            } else {
+                // Success - the entity was created properly
+                alert("Success!\nUUID of created Entity: "
+                    + JSON.stringify(response.get('uuid')));
+                console.log(response);
+
+                //homepage.categories[] = prob.category;
+            }
+        });
     }
 
-    function deleteProblem(problem) {
-        var client = getClient();
+    function deleteProblem(prob) {
+        var properties = {
+            client: dataClient,
+            data: {
+                type: PROBLEMS_TYPE,
+                uuid: prob.uuid
+            }
+        };
+
+        // create a local Entity object that we can call destroy() on
+        var entity = new Apigee.Entity(properties);
+
+        // call the destroy() method to initiate the API DELETE request */
+        entity.destroy(function (error, response) {
+            if (error) {
+                // Error - there was a problem creating the entity
+                alert("Error! Unable to create your entity. "
+                    + "See console to view detailed error message.");
+                console.log(error);
+            } else {
+                // Success - the entity was successfully deleted
+                alert("Success! The entity has been deleted.");
+                console.log(response);
+            }
+        });
     }
 
     /****************************************************
@@ -84,6 +154,7 @@
             restrict: 'E',
             templateUrl: 'views/add-problems.html'
         };
+
     });
 
     problemListApp.directive('searchProblem', function () {
@@ -94,3 +165,38 @@
     });
 
 })();
+
+var formatProblem = function (problem) {
+    var parser = document.createElement('a');
+    parser.href = problem.link;
+
+    var query = parseQueryString(parser.search);
+    switch (parser.hostname) {
+        case "uva.onlinejudge.org":
+            problem.name = "UVA " + query["problem"];
+            break;
+        default:
+            break;
+    }
+
+    /*
+     parser.protocol; // => "http:"
+     parser.hostname; // => "example.com"
+     parser.port;     // => "3000"
+     parser.pathname; // => "/pathname/"
+     parser.search;   // => "?search=test"
+     parser.hash;     // => "#hash"
+     parser.host;     // => "example.com:3000"
+     */
+};
+
+var parseQueryString = function (str) {
+    var objURL = {};
+    str.replace(
+        new RegExp("([^?=&]+)(=([^&]*))?", "g"),
+        function ($0, $1, $2, $3) {
+            objURL[$1] = $3;
+        }
+    );
+    return objURL;
+};
