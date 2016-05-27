cdfc37e8ed29afad9bfb67d9937b8ba6a426f261
dipu-bd <dipu.sudipta@gmail.com>
2016-05-27 13:50:28 +0600

    build login page

--- js/app.js
+++ js/app.js
@@ -1,62 +1,4 @@
 /***************************************************
- * APIGEE Client
- ****************************************************/
-var APIGEE_CLIENT = {
-
-    client: new Apigee.Client({
-        orgName: 'sdipu',
-        appName: 'problemsolvinglist'
-    }),
-
-    catmap: {},
-    problems: [],
-    loggedIn: false,
-
-    login: function (user) {
-        var $scope = this;
-        client.login(user.username, user.password, function (error, res) {
-            if (error) {
-                displayMessage(error);
-            }
-            else {
-                $scope.loggedIn = true;
-                update();
-            }
-        });
-    },
-
-    logout: function () {
-        client.logout();
-        this.loggedIn = false;
-        this.problems = [];
-    },
-
-    update: function () {
-        var prop = {
-            type: 'problems'
-        };
-        var $scope = this;
-        setTimeout(function () {
-            client.getEntity(prop, function (error, response) {
-                if (error) {
-                    console.log(error);
-                } else {
-                    $scope.problems = response.entities;
-                    $scope.catmap = {};
-                    for(var i = 0; i < $scope.problems.length; ++i) {
-                        $scope.catmap[$scope.problems[i].category] = true;
-                    }
-                }
-            });
-        }, 100);
-    },
-
-    categories: function () {
-        return Object.getOwnPropertyNames(this.catmap);
-    }
-};
-
-/***************************************************
  * ANGULAR controllers and directives
  ****************************************************/
 (function () {
@@ -64,18 +6,18 @@ var APIGEE_CLIENT = {
     var app = angular.module('ProblemListApp', []);
 
     var homepage = this;
-    var apigee = new APIGEE_CLIENT();
 
     /***************************************************
      * HOME PAGE CONTROLLER
      ****************************************************/
     app.controller('IndexController', function ($scope) {
         homepage = $scope;
-        $scope.config = getConfig();
+        homepage.config = getConfig();
 
-        $scope.loggedIn = apigee.loggedIn;
-        $scope.problems = apigee.problems;
-        $scope.update = apigee.update;
+        // initialize apigee client
+        homepage.catmap = {};
+        homepage.problems = [];
+        homepage.loggedIn = false;
     });
 
     /***************************************************
@@ -86,7 +28,8 @@ var APIGEE_CLIENT = {
             restrict: 'E',
             templateUrl: 'forms/login-form.html',
             controller: function ($scope) {
-                $scope.login = apigee.login;
+                $scope.client = {};
+                $scope.login = login;
             }
         };
     });
@@ -95,7 +38,6 @@ var APIGEE_CLIENT = {
             restrict: 'E',
             templateUrl: 'forms/add-problem.html',
             controller: function ($scope) {
-                
             }
         };
     });
@@ -104,7 +46,6 @@ var APIGEE_CLIENT = {
             restrict: 'E',
             templateUrl: 'forms/edit-problem.html',
             controller: function ($scope) {
-
             }
         };
     });
@@ -114,7 +55,7 @@ var APIGEE_CLIENT = {
             restrict: 'E',
             templateUrl: 'elements/top-bar.html',
             controller: function ($scope) {
-                $scope.logout = apigee.logout();
+                $scope.logout = logout;
             }
         };
     });
@@ -135,7 +76,9 @@ var APIGEE_CLIENT = {
             restrict: 'E',
             templateUrl: 'elements/category-list.html',
             controller: function ($scope) {
-                $scope.categories = apigee.categories;
+                $scope.categories = function () {
+                    return Object.getOwnPropertyNames(homepage.catmap);
+                };
             }
         };
     });
@@ -144,10 +87,66 @@ var APIGEE_CLIENT = {
             restrict: 'E',
             templateUrl: 'elements/problem-list.html',
             controller: function ($scope) {
-                $scope.problems = apigee.problems;
             }
         };
     });
+
+    /***************************************************
+     * APIGEE Client
+     ****************************************************/
+
+    var client = new Apigee.Client({
+        orgName: 'sdipu',
+        appName: 'problemsolvinglist'
+    });
+
+    function login(user) {
+        client.login(user.username, user.password, function (error, res, user) {
+            if (error) {
+                displayMessage(error);
+            }
+            else {
+                homepage.user = user._data;
+                afterLogin();
+            }
+        });
+    }
+
+    function logout() {
+        client.logout();
+        afterLogout();
+    }
+
+    function update() {
+        var $scope = homepage;
+        var prop = {
+            type: 'problems'
+        };
+        setTimeout(function () {
+            client.getEntity(prop, function (error, response) {
+                if (error) {
+                    console.log(error);
+                } else {
+                    $scope.problems = response.entities;
+                    $scope.catmap = {};
+                    for (var i = 0; i < $scope.problems.length; ++i) {
+                        $scope.catmap[$scope.problems[i].category] = true;
+                    }
+                }
+            });
+        }, 100);
+    }
+
+    function afterLogin() {
+        homepage.loggedIn = true;
+        update();
+    }
+
+    function afterLogout() {
+        homepage.user = {};
+        homepage.problems = [];
+        homepage.loggedIn = false; 
+    }
 
 })();
 
